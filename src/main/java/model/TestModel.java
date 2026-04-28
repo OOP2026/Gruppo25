cb43b2435c47f9ec990421163ed724fb8558d57b
framarinaro <fr.marinaro@studenti.unina.it>
2026-04-28 19:54:53 +0200

    Aggiunta Main di prova
    E' stato aggiunto un piccolo main di prova che stampa la matricola dello Studente.

--- src/main/java/model/TestModel.java
+++ src/main/java/model/TestModel.java
@@ -3,7 +3,8 @@ package model;
 public class TestModel {
 
 	public static void main(String[] args) {
-		//la classe è al momento vuota.
+		Studente f = new Studente("francesco", "DD!!", "francesco pio" , "marinaro", "fr@gmail.com", "informatica", "DE10000");
+		System.out.println(f.getMatricola());
 	}
 
 }
