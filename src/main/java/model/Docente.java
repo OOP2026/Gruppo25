85ee3f2fe2f851754c69c3bc28dfa10e5a0775ca
Domenico-dibiase <do.dibiase@studenti.unina.it>
2026-05-15 19:20:03 +0200

    Aggiornamenti minori
    modifiche ArrayList in List

--- src/main/java/model/Docente.java
+++ src/main/java/model/Docente.java
@@ -10,42 +10,29 @@ public class Docente extends Utente {
     private List<Tesi> tesi = new ArrayList<>();
     private List<RichiestaTirocinio> richiestaTirocinio = new ArrayList<>();
 
-    //Costrutture dell'Oggetto Docente
+    //costrutture dell'oggetto Docente
     public Docente(String login, String password, String nome, String cognome, String email, String corsoLaurea,String argomentiTirocinio) {
         super(login, password, nome, cognome, email, corsoLaurea);
         this.argomentiTirocinio.add(argomentiTirocinio);
     }
 
-    // Implementazione dei vari metodi get e set di tutti gli attributi della classe
-
-    public List<String> getArgomentiTirocinio() {return argomentiTirocinio;}
-
-    public void setArgomentiTirocinio(List<String> argomentiTirocinio) {this.argomentiTirocinio = argomentiTirocinio;}
-
-    public List<Tirocinio> getTirocinio() {return tirocinio;}
-
-    public void setTirocinio(List<Tirocinio> tirocinio) {this.tirocinio = tirocinio;}
-
+    public List<Tirocinio> getTirocinio() { return tirocinio; }
+    public void setTirocinio(ArrayList<Tirocinio> tirocinio) { this.tirocinio = tirocinio; }
     public void addTirocinio(Tirocinio nuovoTirocinio) {
         if (nuovoTirocinio != null) {
             this.tirocinio.add(nuovoTirocinio);
         }
     }
 
-    public List<Tesi> getTesi() {return tesi;}
-
-    public void setTesi(List<Tesi> tesi) {this.tesi = tesi;}
-
+    public List<Tesi> getTesi() { return tesi; }
     public void addTesi(Tesi nuovaTesi) {
         if (nuovaTesi != null) {
             this.tesi.add(nuovaTesi);
         }
     }
 
-    public List<RichiestaTirocinio> getRichiestaTirocinio() {return richiestaTirocinio;}
-
-    public void setRichiestaTirocinio(List<RichiestaTirocinio> richiestaTirocinio) {this.richiestaTirocinio = richiestaTirocinio;}
-
+    public List<RichiestaTirocinio> getRichiestaTirocinio() { return richiestaTirocinio; }
+    public void setRichiestaTirocinio(ArrayList<RichiestaTirocinio> richiestaTirocinio) { this.richiestaTirocinio = richiestaTirocinio; }
 }
 
 
