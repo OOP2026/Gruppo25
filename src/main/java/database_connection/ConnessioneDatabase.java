9cd2709b7e935124ce83591485f61fba90101af6
Domenico-dibiase <do.dibiase@studenti.unina.it>
2026-06-26 12:28:03 +0200

    Update ConnessioneDatabase.java

--- src/main/java/database_connection/ConnessioneDatabase.java
+++ src/main/java/database_connection/ConnessioneDatabase.java
@@ -1,5 +1,39 @@
 package database_connection;
 
-public class ConnessioneDatabase {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnessioneDatabase {
+    private static ConnessioneDatabase instance;
+    private Connection connection = null;
+
+    private String url = "jdbc:postgresql://ep-sweet-sun-ab4tkwsd-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channelBinding=require";
+    private String user = "neondb_owner";
+    private String password = "npg_vxd3IToV0Sup";
+    private String driver = "org.postgresql.Driver";
+
+    private ConnessioneDatabase() throws SQLException {
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Database Connection Creation Failed: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    public static ConnessioneDatabase getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new ConnessioneDatabase();
+        }
+        else if(instance.getConnection().isClosed()){
+            instance = new ConnessioneDatabase();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
